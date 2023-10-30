e50a7786d7246605344301f1bfbda609d2abb241
JRaghu842 <jraghavendra842@gmail.com>
2023-10-30 15:26:13 +0530

    cors issue fixed

--- routes/cors_prevent.routes.js
+++ routes/cors_prevent.routes.js
@@ -3,13 +3,26 @@ let corsRouter = express.Router();
 require("dotenv").config();
 
 corsRouter.get("/proxy", async (req, res) => {
-  const response = await fetch(req.query.url, {
-    headers: {
-      Authorization: `Bearer ${process.env.INCH_API}`,
-    },
-  });
-  const data = await response.json();
-  res.json(data);
+  try {
+    const fetch = (await import("node-fetch")).default; // Dynamic import of node-fetch
+
+    const response = await fetch(req.query.url, {
+      headers: {
+        Authorization: `Bearer ${process.env.INCH_API}`,
+      },
+    });
+
+    if (!response.ok) {
+      // Check if response status is not OK (not 2xx)
+      throw new Error(`HTTP Error ${response.status}: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send({ error: "Failed to fetch data." });
+  }
 });
 
 module.exports = {
